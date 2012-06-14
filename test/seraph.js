a0e4fd0e7c504c7d512d949106083c734562337b
Jon Packer <contact@jonpacker.com>
2012-06-14 17:22:58 +0200

    queries now detect and return whole objects if found. closes #12

--- test/seraph.js
+++ test/seraph.js
@@ -490,6 +490,77 @@ describe('seraph#query, seraph#queryRaw', function() {
     async.waterfall([createObjs, linkObjs, query], done);
   });
 
+  it('should perform a cypher query and return whole objects', function(done) {
+    function createObjs(done) {
+      db.save([{name: 'Jon', age: 23}, 
+               {name: 'Neil', age: 60},
+               {name: 'Katie', age: 29}], function(err, users) {
+        done(null, users[0], users.slice(1));
+      });
+    }
+
+    function linkObjs(user1, users, done) {
+      db.relate(user1, 'knows', users, function(err, links) {
+        done(null, user1, users);
+      });
+    }
+
+    function query(user, users, done) {
+      var cypher = "start x = node(" + user.id + ") ";
+      cypher    += "match x -[r]-> n ";
+      cypher    += "return n ";
+      cypher    += "order by n.name";
+      db.query(cypher, function(err, result) {
+        assert.ok(!err);
+        assert.deepEqual([ users[1], users[0] ], result);
+        done();
+      });
+    }
+  
+    async.waterfall([createObjs, linkObjs, query], done);
+  });
+
+  it('should perform a cypher query and return rels', function(done) {
+    function createObjs(done) {
+      db.save([{name: 'Jon', age: 23}, 
+               {name: 'Neil', age: 60},
+               {name: 'Katie', age: 29}], function(err, users) {
+        done(null, users[0], users.slice(1));
+      });
+    }
+
+    function linkObjs(user1, users, done) {
+      db.relate(user1, 'knows', users, function(err, links) {
+        done(null, user1, users, links);
+      });
+    }
+
+    function query(user, users, links, done) {
+      var cypher = "start x = node(" + user.id + ") ";
+      cypher    += "match x -[r]-> n ";
+      cypher    += "return r ";
+      cypher    += "order by n.name";
+      db.query(cypher, function(err, result) {
+        assert.ok(!err);
+        assert.deepEqual([
+          { start: user.id, 
+            end: users[1].id, 
+            type: 'knows', 
+            properties: {},
+            id: links[1].id },
+          { start: user.id, 
+            end: users[0].id, 
+            type: 'knows', 
+            properties: {},
+            id: links[0].id }
+        ], result);
+        done();
+      });
+    }
+  
+    async.waterfall([createObjs, linkObjs, query], done);
+  });
+
   it('should perform a cypher query w/o parsing the result', function(done) {
     function createObjs(done) {
       db.save([{name: 'Jon', age: 23}, 
