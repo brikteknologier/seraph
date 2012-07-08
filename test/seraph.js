ee8edec1c4fca50f569863d517bef6467838a665
Jon Packer <contact@jonpacker.com>
2012-07-08 02:23:05 +0200

    added test for rel.delete

--- test/seraph.js
+++ test/seraph.js
@@ -342,6 +342,38 @@ describe('seraph.rel', function() {
     async.waterfall([createObjs, linkObjs, readLink], done);
   });
 
+  it('should delete a relationship', function(done) {
+    function createObjs(done) {
+      db.save([{name: 'Jon'}, {name: 'Helge'}], function(err, users) {
+        done(null, users[0], users[1]);
+      });
+    }
+
+    function linkObjs(user1, user2, done) {
+      db.rel.create(user1, 'coworker', user2, function(err, link) {
+        done(null, link, user1, user2);
+      });
+    }
+
+    function delLink(link, user1, user2, done) {
+      var linkId = link.id;
+      db.rel.read(link.id, function(err, link) {
+        assert.equal(link.start, user1.id);
+        assert.equal(link.end, user2.id);
+        db.rel.delete(link.id, function(err) {
+          assert.ok(!err);
+          db.rel.read(link.id, function(err, link) {
+            assert.ok(!!err);
+            assert.ok(!link);
+          })
+        })
+        done(null);
+      });
+    }
+
+    async.waterfall([createObjs, linkObjs, delLink], done);
+  });
+
   it('should link two objects with props on the link', function(done) {
     function createObjs(done) {
       db.save([{name: 'Jon'}, {name: 'Helge'}], function(err, users) {
