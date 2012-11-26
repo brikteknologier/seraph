a257ed1009e768a049b8491404d6d5fb234be664
Helge Skogly Holm <helge.holm@gmail.com>
2012-11-26 14:35:11 +0100

    Testing server alias even more explicitly.

--- test/seraph.js
+++ test/seraph.js
@@ -85,25 +85,35 @@ after(stopDb);
 
 describe('configuration', function() {
   it('should understand ids when ref the server by alias', function(done) {
-    var alias = 'http://127.0.0.1:' + TEST_INSTANCE_PORT;
-    var db = _seraph(alias);
-    var idxName = uniqn();
-    var origNode = { jelly: "belly" };
-    function mkNode(done) {
-      db.save(origNode, done);
+    /* This checks that the server accepts seraph constructed node id
+     * urls where the authority segment or the URL refers to the
+     * server by a different name than it knows itself as. */
+    function testWithServerName(serverName, done) {
+      var alias = 'http://' + serverName + ':' + TEST_INSTANCE_PORT;
+      var db = _seraph(alias);
+      var idxName = uniqn();
+      var origNode = { jelly: "belly" };
+      function mkNode(done) {
+        db.save(origNode, done);
+      }
+      function mkIdx(node, done) {
+        db.index(idxName, node.id, 'application', node.jelly, done);
+      }
+      function readIdx(done) {
+        db.node.index.read(idxName, 'application',
+                           origNode.jelly, done);
+      }
+      function check(nodeFromIndex, done) {
+        assert.equal(nodeFromIndex.jelly, origNode.jelly);
+        done();
+      }
+      async.waterfall([mkNode, mkIdx, readIdx, check], done);
     }
-    function mkIdx(node, done) {
-      db.index(idxName, node.id, 'application', node.jelly, done);
-    }
-    function readIdx(done) {
-      db.node.index.read(idxName, 'application',
-                         origNode.jelly, done);
-    }
-    function check(nodeFromIndex, done) {
-      assert.equal(nodeFromIndex.jelly, origNode.jelly);
-      done();
-    }
-    async.waterfall([mkNode, mkIdx, readIdx, check], done);
+
+    testWithServerName('127.0.0.1', function(err) {
+      if (err) return done(err);
+      testWithServerName('localhost', done);
+    });
   });
 });
 
