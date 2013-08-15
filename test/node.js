30ea6f910abd309586f3dc802bc1ccca1b6909b0
Jon Packer <contact@jonpacker.com>
2013-08-15 12:59:22 +0200

    Added single property update tests

--- test/node.js
+++ test/node.js
@@ -103,6 +103,21 @@ describe('seraph#save, seraph#read', function() {
     async.waterfall([createObjs, readObjs], done);
   });
 
+  it('should save a single property of an object', function(done) {
+    db.save({name: 'bob', age: 46}, function(err, node) {
+      assert(!err)
+      assert.equal(node.name, 'bob');
+      assert.equal(node.age, 46);
+      node.name = 'hidden name man';
+      db.save(node, 'age', 47, function(err, node) {
+        assert(!err);
+        assert.equal(node.name, 'bob')
+        assert.equal(node.age, 47);
+        done();
+      });
+    });
+  });
+
   it('should handle an empty array of objects to save', function(done) {
     db.save([], done);
   });
