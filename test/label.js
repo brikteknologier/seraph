30c194bc1969c4b2c8fbb62b653eea17be264299
Jon Packer <contact@jonpacker.com>
2013-10-30 17:10:10 +0100

    labels: added basic save/read tests

---
+++ test/label.js
@@ -0,0 +1,32 @@
+var testDatabase = require('./util/database');
+var db = require('../')(testDatabase.url);
+var uniqn = require('./util/ponies').uniqn;
+
+var assert = require('assert');
+var async = require('async');
+
+describe('seraph#label', function() {
+  it('should be able to add a label to a node', function(done) {
+    var label = uniqn();
+    db.save({ name: 'Jon' }, function(err, node) {
+      assert(!err);
+      assert(node.id);
+      db.label(node, label, done);
+    });   
+  });
+
+  it('should be able to read all nodes with a label', function(done) {
+    var label = uniqn();
+    db.save({ name: 'Jon' }, function(err, node) {
+      assert(!err);
+      assert(node.id);
+      db.label(node, label, function(err) {
+        assert(!err);
+        db.readLabel(label, function(err, results) {
+          assert(!err);
+          assert.deepEqual(results[0], node);
+        });
+      });
+    });   
+  });
+});
