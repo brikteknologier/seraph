2a2a5c26915a237c6a1819a7b306293aad34721e
Jon Packer <contact@jonpacker.com>
2014-06-22 22:53:20 +0200

    update test versions.

--- test/util/database.js
+++ test/util/database.js
@@ -11,7 +11,7 @@ var _nsv;
 
 var refreshDb = function(done) {
   disposableSeraph({
-      version: '2.0.3', 
+      version: '2.1.2', 
       edition: 'community', 
       port: TEST_INSTANCE_PORT 
     },
