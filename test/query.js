03dd41b29660604db74ea994c4610d4bb4f733a8
horyd <prods3@gmail.com>
2014-09-09 23:29:27 +1000

    Parsing literal map test added

--- test/query.js
+++ test/query.js
@@ -157,6 +157,36 @@ describe('seraph#query, seraph#queryRaw', function() {
     async.waterfall([createObjs, linkObjs, query], done);
   });
 
+  it('should perform a cypher query and parse nodes within literal map results', function(done) {
+    function createObjs(done) {
+      db.save([{name: 'Jon', age: 23}, 
+               {name: 'Neil', age: 60},
+               {name: 'Katie', age: 29}], function(err, users) {
+        done(null, users[0], users.slice(1));
+      });
+    }
+
+    function linkObjs(user1, users, done) {
+      db.relate(user1, 'knows', users, function(err, links) {
+        done(null, user1, users);
+      });
+    }
+
+    function query(user, users, done) {
+      var cypher = "start x = node(" + user.id + ") ";
+      cypher    += "match x -[r]-> n ";
+      cypher    += "return {data: n} as user ";
+      cypher    += "order by user.data.name";
+      db.query(cypher, function(err, result) {
+        assert.ok(!err);
+        assert.deepEqual([ {data: users[1]}, {data: users[0]} ], result);
+        done();
+      });
+    }
+  
+    async.waterfall([createObjs, linkObjs, query], done);
+  });
+
   it('should perform a cypher query and return rels', function(done) {
     function createObjs(done) {
       db.save([{name: 'Jon', age: 23}, 
