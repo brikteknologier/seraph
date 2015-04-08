875381e2c3b1d4bb0f88af466069f45a05d7d69f
Jon Packer <contact@jonpacker.com>
2015-04-08 12:08:23 +0200

    auth: change password on test setup. also acts as a test for changePassword

--- test/root.js
+++ test/root.js
@@ -1,6 +1,10 @@
 /* -*- Mode: Javascript; js-indent-level: 2 -*- */
 
 var testDatabase = require('./util/database');
+var db = require('../')(testDatabase.url);
 
 before(testDatabase.refreshDb);
+before(function(done) {
+  db.changePassword('test', done);
+});
 after(testDatabase.stopDb);
