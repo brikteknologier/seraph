c28bd55fbc98a238c218caf6ef682ac7e8cba82c
Jon Packer <contact@jonpacker.com>
2015-06-11 22:22:05 +0200

    thunkify library using thunkify option or require("seraph/co")

--- co.js
+++ co.js
@@ -1,5 +1,20 @@
 var seraph = require('./lib/seraph');
+var thunkify = require('thunkify');
+var excludes = [
+  'batch',
+  'operation'
+];
+
 module.exports = function() {
   var db = seraph.apply(null, [].slice.call(arguments));
-  console.log(db);
+  return function wrapObject(obj) {
+    Object.keys(obj).forEach(function(key) {
+      if (typeof obj[key] == 'function' && excludes.indexOf(key) == -1 && key[0] != '_') {
+        obj[key] = thunkify(obj[key]);
+      } else if (typeof obj[key] == 'object') {
+        obj[key] = wrapObject(obj[key]);
+      }
+    }); 
+    return obj;
+  }(db);
 };
